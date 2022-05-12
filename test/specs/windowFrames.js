9b7d1346fd7204981c4461225a5de303c9adc380
abhmora1011 <59785830+abhmora1011@users.noreply.github.com>
2022-05-12 15:36:14 +0800

    Added script for handling windows by switching

--- test/specs/windowFrames.js
+++ test/specs/windowFrames.js
@@ -5,12 +5,16 @@ describe("Windows and Frames Miscellaneaous",async ()=>{
         await browser.maximizeWindow()
         await $(".blinkingText").click()
         const handles = await browser.getWindowHandles() // 2 windows
-        await browser.switchToWindow(handles[1])
+        await browser.switchToWindow(handles[1]) // Dealing with the browser by application
         console.log(await $("h2 span").getText())
         console.log(await browser.getTitle())
         await browser.closeWindow()
         await browser.switchToWindow(handles[0])
         console.log(await browser.getTitle())
-
+        await browser.newWindow("https://google.com/")
+        console.log(await browser.getTitle())
+        await browser.switchWindow("https://rahulshettyacademy.com/loginpagePractise/") // Browser are open by automation
+        await $("#username").setValue("helloIswitchback")
+        await browser.pause(3000)
     })
 })
